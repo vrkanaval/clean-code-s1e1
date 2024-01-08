1e9fb82b3291d453f10a2685585e921dec998459
vrkanaval <veronica22.199@gmail.com>
2024-01-08 22:46:16 +0400

    fix(no rule): add functionality for checkboxes in js file

--- app.js
+++ app.js
@@ -130,6 +130,8 @@ var taskCompleted=function(){
 
   //Append the task list item to the #completed-tasks
   var listItem=this.parentNode;
+  var label=listItem.querySelector("label");
+  label.classList.add("completed-task");
   completedTasksHolder.appendChild(listItem);
   bindTaskEvents(listItem, taskIncomplete);
 
@@ -142,6 +144,8 @@ var taskIncomplete=function(){
   //When the checkbox is unchecked
   //Append the task list item to the #incomplete-asks.
   var listItem=this.parentNode;
+  var label=listItem.querySelector("label");
+  label.classList.remove("completed-task");
   incompleteTaskHolder.appendChild(listItem);
   bindTaskEvents(listItem,taskCompleted);
 }
